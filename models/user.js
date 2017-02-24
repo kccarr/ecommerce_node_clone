6e4369709ba24aed98a65573777502854ed09a91
Kevin Carr <cochewen@gmail.com>
2017-02-24 17:23:09 -0500

    User Schema created with mongoose, password hashing with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
 
 // The user schema attributes, characteristics, fields
 
-var userSchema = new mongoose.Schema({
+var UserSchema = new Schema({
   email: {type: String, unique: true, lowercase: true},
   password: String,
 
@@ -11,14 +12,37 @@ var userSchema = new mongoose.Schema({
   profile: {
     name: {type: String, default: ''},
     picture: {type: String, default: ''}
-  }
-})
+  },
+
+  address: String,
+  history: [{
+    date: Date,
+    paid: {type: Number, default: 0},
+    //item: {type: Schema.Types.ObjectID, ref: ''}
+  }]
+});
 
 
 
 
 // Hash the password before we even save it to the database
-
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 
 // compare password in the database and the one that the user type i
+UserSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var User = require('./models/user');
 
 var app = express();
 
-app.use(morgan('dev'));
+mongoose.connect('secret_password_connection', function(err) {
+  if(err) {
+      console.log(err);
+  } else {
+      console.log('Connected to the database.')
+  }
 
-app.get('/', function(req, res) {
-  var name = 'Arash';
-  res.json("My name is " + name);
 });
 
-app.get('/catname', function(req, res) {
-  res.json("batman");
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/create-user', function(req, res, next) {
+  var user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+  user.save(function(err) {
+    if(err) return next(err);
+
+    res.json('Successfully created a new user');
+  });
 });
 
 app.listen(3000, function(err) {
